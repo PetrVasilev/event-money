27abc0abd7e498afff6ce5cf6d80e138319c6ecf
arri <ewws@mail.ru>
2020-10-30 20:10:55 +0900

    added router

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,13 +1,16 @@
+import 'antd/dist/antd.css'
 import React from 'react'
 import apollo from "./utils/apollo"
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
 import { ApolloProvider } from '@apollo/react-hooks'
+import Login from "./pages/login"
 
 const App = () => {
     return (
         <ApolloProvider client={apollo}>
-            <div>
-                hello
-            </div>
+            <Router>
+                <Route exact path={'/login'} component={Login}/>
+            </Router>
         </ApolloProvider>
     );
 }
