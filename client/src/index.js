aa581df8357ba065afc75e400f3b8849fe93b567
blnaxblachbl <blnaxblachbl@gmail.com>
2020-10-30 19:39:00 +0900

    add icons

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,29 @@ import * as React from 'react'
 import ReactDOM from 'react-dom'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import iconFont from 'react-native-vector-icons/Fonts/Ionicons.ttf'
+import Ionicons from 'react-native-vector-icons/dist/Ionicons'
 
 import Events from './pages/events'
 import Event from './pages/event'
 import CreateEvent from './pages/create-event'
 import CreateSpending from './pages/create-spending'
+
+const iconFontStyles = `@font-face {
+    src: url(${iconFont});
+    font-family: Ionicons;
+  }
+`
+
+const style = document.createElement('style')
+style.type = 'text/css'
+if (style.styleSheet) {
+    style.styleSheet.cssText = iconFontStyles
+} else {
+    style.appendChild(document.createTextNode(iconFontStyles))
+}
+
+document.head.appendChild(style)
 
 const Stack = createStackNavigator()
 
@@ -15,8 +33,18 @@ const App = () => {
         <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen
-                    options={{
-                        headerTitle: 'Мероприятия'
+                    options={({ navigation }) => {
+                        return {
+                            headerTitle: 'Мероприятия',
+                            headerRight: () => (
+                                <Ionicons
+                                    onPress={() => navigation.navigate("CreateEvent")}
+                                    name="add-circle-outline"
+                                    style={{ color: '#9F8FFF', marginRight: 10 }}
+                                    size={27}
+                                />
+                            )
+                        }
                     }}
                     name="Events"
                     component={Events}
