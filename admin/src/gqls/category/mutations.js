4ae5658ff820436ee940febea9f38bc5f6607fce
arri <ewws@mail.ru>
2020-10-31 00:11:21 +0900

    add category

---
+++ admin/src/gqls/category/mutations.js
@@ -0,0 +1,13 @@
+import gql from 'graphql-tag'
+
+export const CREATE_ONE_CATEGORY = gql`
+    mutation ($data:CategoryCreateInput!){
+        createOneCategory(data: $data){
+            name
+            id
+            updatedAt
+            createdAt
+        }
+        
+    }
+`
