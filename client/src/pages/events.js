882ae58a2de9a812de0091a30df6fb132cbdf479
Petr Vasilev <vpa17@ya.ru>
2020-10-30 19:18:01 +0900

    fixs

--- client/src/pages/events.js
+++ client/src/pages/events.js
@@ -1,13 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const View = styled.div``
+const List = styled.div``
+
+const Event = styled.div``
+
+const events = [
+    {
+        id: 1,
+        name: 'Свадьба Васильевых',
+        category: 'Свадьба',
+        date: new Date(),
+        created: new Date()
+    }
+]
 
 const Events = () => {
     return (
-        <View>
-            
-        </View>
+        <List>
+            {events.map((item) => (
+                <Event key={item.id}>
+                    <div className="name">{item.name}</div>
+                    <div className="category">{item.category}</div>
+                </Event>
+            ))}
+        </List>
     )
 }
 
