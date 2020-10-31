050231832dd8fd3339b55e7701d10853228e7485
Petr Vasilev <vpa17@ya.ru>
2020-11-01 00:24:40 +0900

    fix

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,67 +1,60 @@
 import 'antd/dist/antd.css'
-import React, {useEffect, useState} from 'react'
-import apollo from "./utils/apollo"
-import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom'
-import {ApolloProvider} from '@apollo/react-hooks'
-import Login from "./pages/login"
-import Sider from "./components/sider"
-import Padding from "./components/padding"
-import AddCategory from "./pages/addCategory"
-import AddService from "./pages/addService"
-import AddTemplate from "./pages/addTemplate"
-import {ADMIN} from "./gqls/auth/queries"
-import Category from "./pages/category"
-import Service from "./pages/service"
-import Template from "./pages/template"
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
+import { ApolloProvider } from '@apollo/react-hooks'
+
+import apollo from './utils/apollo'
+import Login from './pages/login'
+import Sider from './components/sider'
+import Padding from './components/padding'
+import AddCategory from './pages/addCategory'
+import AddService from './pages/addService'
+import AddTemplate from './pages/addTemplate'
+import { ADMIN } from './gqls/auth/queries'
+import Category from './pages/category'
+import Service from './pages/service'
+import Template from './pages/template'
 
 const App = () => {
     const [loading, setLoading] = useState(true)
     const [admin, setAdmin] = useState()
-    useEffect(
-        () => {
-            apollo.watchQuery({
+    useEffect(() => {
+        apollo
+            .watchQuery({
                 query: ADMIN,
                 fetchPolicy: 'cache-and-network',
                 errorPolicy: 'ignore'
-            }).subscribe({
+            })
+            .subscribe({
                 next: (res) => {
-                    const {data, loading} = res
-                    if (!loading)
-                        setAdmin(data.admin)
+                    const { data, loading } = res
+                    if (!loading) setAdmin(data.admin)
                     setLoading(loading)
-
                 }
             })
-        },
-        []
-    )
+    }, [])
 
     return (
         <ApolloProvider client={apollo}>
             <Router>
-                {
-                    !loading && !admin ?
-                        <Redirect to={'/login'}/>
-                        :
-                        null
-                }
-                <Route exact path={'/login'} component={Login}/>
+                {!loading && !admin ? <Redirect to={'/login'} /> : null}
+                <Route exact path={'/login'} component={Login} />
                 <Route exact path="/authorized/:path?/">
-                    <Sider/>
+                    <Sider />
                     <Switch>
                         <Padding>
-                            <Route exact path={'/authorized/category'} component={Category}/>
-                            <Route exact path={'/authorized/service'} component={Service}/>
-                            <Route exact path={'/authorized/addCategory'} component={AddCategory}/>
-                            <Route exact path={'/authorized/addService'} component={AddService}/>
-                            <Route exact path={'/authorized/addTemplate'} component={AddTemplate}/>
-                            <Route exact path={'/authorized/template'} component={Template}/>
+                            <Route exact path={'/authorized/category'} component={Category} />
+                            <Route exact path={'/authorized/service'} component={Service} />
+                            <Route exact path={'/authorized/addCategory'} component={AddCategory} />
+                            <Route exact path={'/authorized/addService'} component={AddService} />
+                            <Route exact path={'/authorized/addTemplate'} component={AddTemplate} />
+                            <Route exact path={'/authorized/template'} component={Template} />
                         </Padding>
                     </Switch>
                 </Route>
             </Router>
         </ApolloProvider>
-    );
+    )
 }
 
-export default App;
+export default App
--- client/src/pages/template.js
+++ client/src/pages/template.js
@@ -1,14 +1,5 @@
 import React from 'react'
-import {
-    ScrollView,
-    StyleSheet,
-    Dimensions,
-    View,
-    Text,
-    TouchableOpacity
-} from 'react-native'
-
-const { width, height } = Dimensions.get("screen")
+import { ScrollView, StyleSheet, View, Text, TouchableOpacity } from 'react-native'
 
 const Template = ({ navigation, route }) => {
     const { template, selected, setTemplate } = route.params
@@ -22,10 +13,8 @@ const Template = ({ navigation, route }) => {
         navigation.goBack()
     }
 
-    console.log(template)
-
     return (
-        <ScrollView style={styles.container} contentContainerStyle={{ alignItems: "center" }}>
+        <ScrollView style={styles.container} contentContainerStyle={{ alignItems: 'center' }}>
             <Text style={styles.textInfo}>Общая информация</Text>
             <View style={styles.bottom}>
                 <View style={styles.bottomSpending}>
@@ -43,35 +32,42 @@ const Template = ({ navigation, route }) => {
                     </Text>
                 </View>
             </View>
-            {
-                template.services.length > 0 ? (
-                    <>
-                        <Text style={styles.textInfo}>Услуги</Text>
-                        <View style={styles.bottom}>
-                            {
-                                template.services.map(item => (
-                                    <View key={item}>
-                                        <View style={styles.bottomSpending}>
-                                            <Text style={{ color: 'gray' }}>Название услуги</Text>
-                                            <Text style={{ textAlign: 'right' }}>{item.category.name}</Text>
-                                        </View>
-                                        <View style={[styles.bottomSpending, { marginBottom: 0 }]}>
-                                            <Text style={{ color: 'gray' }}>Цена</Text>
-                                            <Text style={{ textAlign: 'right' }}>{item.amount}</Text>
-                                        </View>
-                                    </View>
-                                ))
-                            }
+            {template.services.length > 0 ? (
+                <>
+                    <Text style={styles.textInfo}>Услуги</Text>
+                    {template.services.map((item, index) => (
+                        <View
+                            style={[
+                                styles.bottom,
+                                { marginBottom: template.services.length - 1 === index ? 0 : 10 }
+                            ]}
+                            key={item}
+                        >
+                            <View style={styles.bottomSpending}>
+                                <Text style={{ color: 'gray' }}>Название услуги</Text>
+                                <Text style={{ textAlign: 'right' }}>{item.category.name}</Text>
+                            </View>
+                            <View style={[styles.bottomSpending, { marginBottom: 0 }]}>
+                                <Text style={{ color: 'gray' }}>Цена</Text>
+                                <Text style={{ textAlign: 'right' }}>{item.amount}</Text>
+                            </View>
                         </View>
-                    </>
-                ) : null
-            }
+                    ))}
+                </>
+            ) : null}
             <TouchableOpacity
                 activeOpacity={0.8}
-                style={[styles.button, selected ? { backgroundColor: "transparent", borderColor: '#4b76a8', borderWidth: 1 } : null]}
+                style={[
+                    styles.button,
+                    selected
+                        ? { backgroundColor: 'transparent', borderColor: '#4b76a8', borderWidth: 1 }
+                        : null
+                ]}
                 onPress={onSubmit}
             >
-                <Text style={[styles.buttonText, selected ? { color: "#4b76a8" } : null]}>{selected ? "Отказаться от шаблона" : "Выбрать шаблон"}</Text>
+                <Text style={[styles.buttonText, selected ? { color: '#4b76a8' } : null]}>
+                    {selected ? 'Отказаться от шаблона' : 'Выбрать шаблон'}
+                </Text>
             </TouchableOpacity>
         </ScrollView>
     )
@@ -79,18 +75,17 @@ const Template = ({ navigation, route }) => {
 
 const styles = StyleSheet.create({
     container: {
-        width,
-        height: height - 60,
-        backgroundColor: '#fafafa',
+        height: window.innerHeight - 60,
+        backgroundColor: '#fafafa'
     },
     textInfo: {
         fontSize: 14,
         color: 'gray',
         marginVertical: 10,
-        width: "90%"
+        width: '90%'
     },
     bottom: {
-        width: "90%",
+        width: '90%',
         backgroundColor: 'white',
         padding: 10,
         borderColor: 'rgb(216, 216, 216)',
