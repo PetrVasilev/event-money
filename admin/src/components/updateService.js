29366d0c126750e4c439a5c7251ef57d16c263f3
Petr Vasilev <vpa17@ya.ru>
2020-11-01 11:20:34 +0900

    fix

--- admin/src/components/updateService.js
+++ admin/src/components/updateService.js
@@ -1,11 +1,11 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
-import {Button, Form, Input, InputNumber, message, Popconfirm, Select} from 'antd'
-import {useMutation, useQuery} from '@apollo/react-hooks'
+import { Button, Form, Input, InputNumber, message, Popconfirm, Select } from 'antd'
+import { useMutation, useQuery } from '@apollo/react-hooks'
 
-import {FIND_MANY_CATEGORY} from '../gqls/category/queries'
-import {DELETE_ONE_SERVICE, UPDATE_ONE_SERVICE} from '../gqls/service/mutations'
-import {FIND_MANY_SERVICE} from '../gqls/service/queries'
+import { FIND_MANY_CATEGORY } from '../gqls/category/queries'
+import { DELETE_ONE_SERVICE, UPDATE_ONE_SERVICE } from '../gqls/service/mutations'
+import { FIND_MANY_SERVICE } from '../gqls/service/queries'
 
 const Container = styled.div`
     display: flex;
@@ -13,22 +13,22 @@ const Container = styled.div`
     max-width: 500px;
 `
 const ButtonsContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
 `
 
-const UpdateService = ({data}) => {
+const UpdateService = ({ data }) => {
     const [categories, setCategories] = useState([])
 
-    const {loading} = useQuery(FIND_MANY_CATEGORY, {
+    const { loading } = useQuery(FIND_MANY_CATEGORY, {
         errorPolicy: 'ignore',
-        onCompleted: ({findManyCategory}) => {
+        onCompleted: ({ findManyCategory }) => {
             setCategories(findManyCategory)
         }
     })
 
-    const [update, {loading: updateLoading}] = useMutation(UPDATE_ONE_SERVICE, {
+    const [update, { loading: updateLoading }] = useMutation(UPDATE_ONE_SERVICE, {
         onError: () => {
             message.error('Что то пошло не так!')
         },
@@ -37,16 +37,16 @@ const UpdateService = ({data}) => {
         }
     })
 
-    const [delItem, {loading: delLoading}] = useMutation(DELETE_ONE_SERVICE, {
+    const [delItem, { loading: delLoading }] = useMutation(DELETE_ONE_SERVICE, {
         onCompleted: async () => {
             message.success('Удалено!')
         },
         onError: () => {
             message.error('Что то пошло не так!')
         },
-        update: (cache, {data: requestData}) => {
+        update: (cache, { data: requestData }) => {
             if (cache && requestData.deleteOneService) {
-                const {findManyService: oldServices} = cache.readQuery({
+                const { findManyService: oldServices } = cache.readQuery({
                     query: FIND_MANY_SERVICE
                 })
                 cache.writeQuery({
@@ -70,14 +70,14 @@ const UpdateService = ({data}) => {
         update({
             variables: {
                 data: {
-                    amount: {set: args.amount.toString()},
+                    amount: { set: args.amount.toString() },
                     category: {
                         connect: {
                             id: args.category
                         }
                     },
-                    description: {set: args.description},
-                    name: {set: args.name}
+                    description: { set: args.description },
+                    name: { set: args.name }
                 },
                 where: {
                     id: data.id
@@ -88,36 +88,36 @@ const UpdateService = ({data}) => {
 
     return (
         <Container>
-            <Form initialValues={{remember: true}} onFinish={submit} layout={'vertical'}>
+            <Form initialValues={{ remember: true }} onFinish={submit} layout={'vertical'}>
                 <Form.Item
                     label="Название"
                     name="name"
                     initialValue={data.name}
-                    rules={[{required: true, message: 'Введите имя!'}]}
+                    rules={[{ required: true, message: 'Введите имя!' }]}
                 >
-                    <Input/>
+                    <Input />
                 </Form.Item>
                 <Form.Item
                     label="Стоимость"
                     name="amount"
                     initialValue={data.amount}
-                    rules={[{required: true, message: 'Введите стоимость!'}]}
+                    rules={[{ required: true, message: 'Введите стоимость!' }]}
                 >
-                    <InputNumber/>
+                    <InputNumber />
                 </Form.Item>
                 <Form.Item
                     label="Описание"
                     name="description"
                     initialValue={data.description}
-                    rules={[{required: true, message: 'Введите описание!'}]}
+                    rules={[{ required: true, message: 'Введите описание!' }]}
                 >
-                    <Input.TextArea/>
+                    <Input.TextArea />
                 </Form.Item>
                 <Form.Item
                     label="Категория"
                     name="category"
-                    initialValue={data.category.id}
-                    rules={[{required: true, message: 'Выберете категорию!'}]}
+                    initialValue={data.category ? data.category.id : undefined}
+                    rules={[{ required: true, message: 'Выберете категорию!' }]}
                 >
                     <Select loading={loading}>
                         {categories.map((item) => {
